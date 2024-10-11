b923076a1151848cffca539538a5cffe3b1c4197
Alícia Stefany <64162281+aliciastefany@users.noreply.github.com>
2024-10-11 14:23:44 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,75 @@
+let menu = window.document.querySelector("#menu-btn");
+let navbar = window.document.querySelector(".navbar");
+
+menu.onclick = () => {
+    menu.classList.toggle("fa-times");
+    navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+    menu.classList.remove("fa-times");
+    navbar.classList.remove("active");
+};
+
+var swiper = new Swiper(".course-slider", {
+    spaceBetween: 20,
+    grabCursor:true,
+    loop:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+       540: {
+         slidesPerView: 1,
+       },
+       768: {
+         slidesPerView: 2,
+       },
+       1024: {
+         slidesPerView: 3,
+       },
+    },
+ });
+
+ var swiper = new Swiper(".teachers-slider", {
+    spaceBetween: 20,
+    grabCursor:true,
+    loop:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+       540: {
+         slidesPerView: 1,
+       },
+       768: {
+         slidesPerView: 2,
+       },
+       1024: {
+         slidesPerView: 3,
+       },
+    },
+ });
+
+ var swiper = new Swiper(".artigo-slider", {
+    spaceBetween: 20,
+    grabCursor:true,
+    loop:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+       540: {
+         slidesPerView: 1,
+       },
+       768: {
+         slidesPerView: 2,
+       },
+       1024: {
+         slidesPerView: 3,
+       },
+    },
+ });
